454e1b3487d71c9f32430032b90b1d3039adebf2
Michael Stafford <michaelstafford808@gmail.com>
2022-09-14 12:28:13 -1000

    made login page with a login form

--- src/authentication/Login.jsx
+++ src/authentication/Login.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login Here</div>
+    <div className="container"> 
+    <h2>MOVIE TRACKER</h2>
+    <div className="app-wrapper">
+      <div>
+        <h2 className="title">Account Login</h2>
+      </div>
+      <form className="form-wrapper">
+        <div className="username">
+            <label className="label">Username</label>
+            <input 
+            className="input" 
+            type="text" 
+            />
+        </div>
+        <div className="password">
+            <label className="label">Password</label>
+            <input 
+            className="input" 
+            type="password"
+            />
+        </div>
+          <input className="submit" type="submit" value="Login" />
+      </form>
+    </div>
+  </div>
   )
 }
 
