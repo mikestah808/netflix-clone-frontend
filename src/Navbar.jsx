28780a1b3d5aee062714969e0f1bbed95aafa531
Michael Stafford <michaelstafford808@gmail.com>
2023-04-13 09:39:06 -1000

    added Link Component to NavBar component to handle navigation

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,7 +19,7 @@ function NavBar() {
     if(user.id){
       return (
         <body
-    class="
+    className="
       antialiased
       bg-gradient-to-r
       from-pink-300
@@ -29,7 +29,7 @@ function NavBar() {
   >
   <header>
      <nav
-        class="
+        className="
           flex flex-wrap
           items-center
           justify-between
@@ -41,9 +41,9 @@ function NavBar() {
           bg-white
         "
       >
-        <div class="hidden w-full md:flex md:items-center md:w-auto " id="menu">
+        <div className="hidden w-full md:flex md:items-center md:w-auto " id="menu">
           <ul
-            class="
+            className="
               text-base text-gray-700
               pt-4
               md:flex
@@ -51,16 +51,16 @@ function NavBar() {
               md:pt-0"
           >
             <li>
-              <button class="md:p-4 py-2 block hover:text-purple-400" to="/" component={ Link } >Home</button>
+              <Link className="md:p-4 py-2 block hover:text-purple-400" to="/">Home</Link>
             </li>
             <li>
-              <button class="md:p-4 py-2 block hover:text-purple-400" to="/genres" component={ Link }>My Genres</button>
+              <Link className="md:p-4 py-2 block hover:text-purple-400" to="/genres">My Genres</Link>
             </li>
             <li>
-              <button class="md:p-4 py-2 block hover:text-purple-400" to="/movies" component={ Link }>My Movies</button>
+              <Link className="md:p-4 py-2 block hover:text-purple-400" to="/movies">My Movies</Link>
             </li>
             <li>
-              <button class="md:p-4 py-2 block hover:text-purple-400 text-purple-500" onClick={handleLogout}>Logout</button>
+              <button className="md:p-4 py-2 block hover:text-purple-400 text-purple-500" onClick={handleLogout}>Logout</button>
             </li>
           </ul>
         </div>
@@ -71,7 +71,7 @@ function NavBar() {
     } else {
       return (
          <body
-    class="
+         className="
       antialiased
       bg-gradient-to-r
       from-pink-300
@@ -81,7 +81,7 @@ function NavBar() {
   >
   <header>
      <nav
-        class="
+        className="
           flex flex-wrap
           items-center
           justify-between
@@ -93,9 +93,9 @@ function NavBar() {
           bg-white
         "
       >
-        <div class="hidden w-full md:flex md:items-center md:w-auto " id="menu">
+        <div className="hidden w-full md:flex md:items-center md:w-auto " id="menu">
           <ul
-            class="
+            className="
               text-base text-gray-700
               pt-4
               md:flex
@@ -103,7 +103,7 @@ function NavBar() {
               md:pt-0"
           >
             <li>
-              <button class="md:p-4 py-2 block hover:text-purple-400 text-purple-500" to="/signup" component={ Link }>Sign Up</button>
+              <button className="md:p-4 py-2 block hover:text-purple-400 text-purple-500" to="/signup" component={ Link }>Sign Up</button>
             </li>
           </ul>
         </div>
